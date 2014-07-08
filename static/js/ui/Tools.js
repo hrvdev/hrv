959fc8fdd0af2af08e5bad0722e0fd05ddb4c849
qfliu <hrvdev@qq.com>
2014-07-08 22:08:57 +0800

    [Feature] Bottom Images

--- static/js/ui/Tools.js
+++ static/js/ui/Tools.js
@@ -6,6 +6,7 @@ var Tools = (function(){
 
     this.dom = $('#tools');
     this.toggleLocationImagesBtn = $('#toggleLocationImagesBtn');
+    this.locationImagesContainer = $('#locationImagesContainer');
     this.init();
   };
 
@@ -17,6 +18,7 @@ var Tools = (function(){
       that.toggleLocationImagesBtn.on('click', function(){
         if(that.toggleLocationImagesBtn.hasClass('open')){
           that.hideLocationImages();
+          that.locationImagesContainer.hide();
           that.miniMap.goToSmall();
         } else {
           that.showLocationImages();
@@ -25,9 +27,84 @@ var Tools = (function(){
       });
       
     },
-    showLocationImages: function(images){
+    showLocationImages: function(){
       this.dom.addClass('open');
       this.toggleLocationImagesBtn.addClass('open');
+
+
+      var that = this;
+      setTimeout(function(){
+        that.renderLocationImages([{
+          src: 'imgs/1.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/2.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/3.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/4.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/1.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/2.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/3.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/4.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/1.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/2.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/3.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/4.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/1.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/2.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/3.jpg',
+          title: 'China'
+        }, {
+          src: 'imgs/4.jpg',
+          title: 'China'
+        }]);
+      }, 200);
+      
+    },
+    renderLocationImages: function(images){
+      this.images = images;
+
+      if(!this.toggleLocationImagesBtn.hasClass('open')){
+        return;
+      }
+
+      var html = [];
+      for(var i = 0; i < images.length; i ++){
+        html.push('<div class="location-image">')
+        html.push('<img src="' + images[i].src + '" />');
+        html.push('<div>' + images[i].title + '</div>');
+        html.push('</div>');
+      }
+      var div = $('<div></div>').html(html.join('')).css('width', images.length * 203 - 3);
+      var that = this;
+      that.locationImagesContainer.html(div).fadeOut(0, function(){
+        that.locationImagesContainer.fadeIn(100);
+      });
     },
     hideLocationImages: function(){
       this.dom.removeClass('open');
